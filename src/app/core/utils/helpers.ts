32bc771d8ee748fc3d8a4b281be38d351c0bbd77
Yaroslav Larin <cyberbiont@gmail.com>
2026-02-26 17:33:14 +0200

    add comment

--- src/app/core/utils/helpers.ts
+++ src/app/core/utils/helpers.ts
@@ -2,7 +2,7 @@ export function getRandomArrayElement<T>(array: T[]): T {
   const element = array.at(Math.floor(Math.random() * array.length));
 
   if (element === undefined) {
-    throw new Error('Cannot get element from an empty array');
+    throw new Error('Cannot get element from an empty array'); // This is just a type guard, bever should happen IRL
   }
   return element;
 }
