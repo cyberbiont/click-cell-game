6fc0c70f10f95486201a4ea9b44da627dfd37cd7
Yaroslav Larin <cyberbiont@gmail.com>
2026-02-26 20:41:17 +0200

    fix config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideBrowserGlobalErrorListeners } from '@angular/core';
 
+import { GlobalErrorHandler } from './core/error-handler/global-error-handler';
 import { provideRouter } from '@angular/router';
 import routes from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,21 @@
 import { Routes } from '@angular/router';
-import { provideGameConfig } from './features/game/game.config';
+import { GAME_CONFIG } from './features/game/game.config';
 
 export default [
   {
     path: '',
     loadComponent: () => import('./features/game/pages/game/game'),
     providers: [
-      provideGameConfig({
-        gridSize: 10,
-        defaultTimeLimitMs: 1000,
-        minTimeLimitMs: 100,
-        maxTimeLimitMs: 2000,
-        winningScore: 10,
-      }),
+      {
+        provide: GAME_CONFIG,
+        useValue: {
+          gridSize: 10,
+          defaultTimeLimitMs: 1000,
+          minTimeLimitMs: 100,
+          maxTimeLimitMs: 2000,
+          winningScore: 10,
+        },
+      },
     ],
   },
 ] satisfies Routes;
